5a936d2939b4406c626e1505d21f6780959a425c
ZoneTwelve <zonetwelve159@gmail.com>
2016-11-19 15:58:48 +0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var iconv = require('iconv-lite');
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-	res.render('index',{output:'Waiting...'});
+router.get('/*', function(req, res, next) {
+	if(req.query.shell&&req.query.encode){
+		shell.exec(req.query.shell,{encoding: 'binary'},function(error,stdout,stderr){
+			res.send(iconv.decode(stdout,req.query.encode));
+		});
+	}else
+		res.render('index',{output:'Waiting...'});
 });
 router.post('/', function(req, res){
 	shell.exec(req.body.shell,{encoding: 'binary'},function(error,stdout,stderr){
-		console.log(req.body.out);
 		if(req.body.out=="here")
 			res.render('index',{output:iconv.decode(stdout,req.body.encode)});
 		else if(req.body.out=="ResultsOnly")
